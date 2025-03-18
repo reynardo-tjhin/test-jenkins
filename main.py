48a3a31627ebc2e59e8b09c3bca2b758c683e72d
Reynardo Tjhin <reychen26@gmail.com>
2025-03-18 23:59:05 +1100

    setup

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from bottle import route, run
+from bottle import route, run, static_file, template, TEMPLATE_PATH
+
+# add the templates folder
+TEMPLATE_PATH.append("./view/templates")
+
+# Serve media files from the 'media' directory
+@route('/media/<filepath:path>')
+def serve_media(filepath):
+    return static_file(filepath, root='./media')
+
 
 @route('/')
 def hello():
-    return "Hello, World!"
+    return template("home")
 
 if (__name__ == "__main__"):
     run(
-        host='localhost',
+        host='192.168.4.51', # 192.168.4.51 or localhost
         port=8080,
         debug=True
     )
